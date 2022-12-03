e5a7ee83be768c1f40084ef96ea0a8b894eee84d
ZCPower <zachpowerxc@gmail.com>
2022-12-02 22:46:32 -0600

    added railway db link for connection

--- db/index.js
+++ db/index.js
@@ -1,8 +1,12 @@
 const { Client } = require('pg');
+const dotenv = require("dotenv").config();
 // imports the pg module
 
+const DB_NAME = 'scheduler';
+const DB_URL =
+    process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 // supply the db name and location of the database
-const client = new Client('postgres://localhost:5432/scheduler');
+const client = new Client(DB_URL);
 
 module.exports = {
     client,
